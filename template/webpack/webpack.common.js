5966b042952ce2c294d897d81609413431d56f14
Jimmie <cross-express@live.com>
2021-11-11 16:38:52 +0800

    cv

--- template/webpack/webpack.common.js
+++ template/webpack/webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const config = require(path.resolve(`${process.cwd()}/xbuild.config.js`));
+const config = require(path.resolve(`${process.cwd()}/max-pack.config.js`));
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
